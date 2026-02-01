46fa4f51a54cef9759a89af1ce3b7693d17a401c
HarshvardhanSrivastavaEEE <harshwardhansrivastava09@gmail.com>
2026-02-01 20:08:06 +0530

    Update insert_element.c

--- DAY01/insert_element.c
+++ DAY01/insert_element.c
@@ -1,26 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
     int n, position, value;
-    int arr[100];
 
     printf("Enter number of elements you want in your array: ");
     scanf("%d", &n);
 
-    printf("Enter elements:\n");
-    for(int i = 0; i < n; i++)
+    
+    int *arr = (int *)malloc((n + 1) * sizeof(int));
+
+    if (arr == NULL)
+    {
+        printf("Memory allocation has failed \n");
+        return 0;
+    }
+
+    printf("Enter %d elements: \n", n);
+    for (int i = 0; i < n; i++)
     {
         scanf("%d", &arr[i]);
     }
 
-    printf("Enter the position you want to insert at: ");
+    printf("Enter position to insert (1 to %d): ", n + 1);
     scanf("%d", &position);
 
-    printf("Enter the value to be inserted: ");
+    if (position < 1 || position > n + 1)
+    {
+        printf("Invalid position\n");
+        free(arr);
+        return 0;
+    }
+
+    printf("Enter value to insert: ");
     scanf("%d", &value);
 
-    for(int i = n; i >= position; i--)
+    
+    for (int i = n; i >= position; i--)
     {
         arr[i] = arr[i - 1];
     }
@@ -29,10 +46,11 @@ int main()
     n++;
 
     printf("Array after insertion:\n");
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         printf("%d ", arr[i]);
     }
 
+    free(arr);   
     return 0;
 }
