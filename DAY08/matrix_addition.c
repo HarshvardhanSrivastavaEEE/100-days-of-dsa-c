2d6aa02bd7e1c5e1cb4e75f6f8696603a98b5997
HarshvardhanSrivastavaEEE <harshwardhansrivastava09@gmail.com>
2026-02-11 20:44:20 +0530

    Create matrix_addition.c

---
+++ DAY08/matrix_addition.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    int m, n;
+    scanf("%d %d", &m, &n);
+
+    int A[m][n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    int B[m][n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d ", A[i][j] + B[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
